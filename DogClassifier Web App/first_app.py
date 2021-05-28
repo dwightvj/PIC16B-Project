e3a0ec28756718de8eec00cc6c51e382000a608b
avaitt <81770510+dwightvj@users.noreply.github.com>
2021-05-28 14:15:14 -0700

    remove redundancies when loading model
    we do not need to compile model and don't care about softmax layer unless we want specific probabilities (based on Erin's discussion)

--- DogClassifier Web App/first_app.py
+++ DogClassifier Web App/first_app.py
@@ -14,8 +14,10 @@
 #######################################################
 import os
 import pathlib
+import skimage
+
 ####*IMPORANT*: Have to do this line *before* importing tensorflow
-os.environ['PYTHONHASHSEED']=str(1)
+os.environ['PYTHONHASHSEED'] = str(1)
 import tensorflow as tf
 from numpy import expand_dims
 from PIL import Image
@@ -28,18 +30,19 @@ import timeit
 import random
 import numpy as np
 import time
+from skimage import measure
+
 
 def reset_random_seeds():
-   os.environ['PYTHONHASHSEED']=str(1)
-   tf.random.set_seed(1)
-   np.random.seed(1)
-   random.seed(1)
+    os.environ['PYTHONHASHSEED'] = str(1)
+    tf.random.set_seed(1)
+    np.random.seed(1)
+    random.seed(1)
+
+
 #######################################################
 
 model = tf.keras.models.load_model('my_model.h5')
-loss = 'categorical_crossentropy'
-optimizer = tf.keras.optimizers.Adam(lr=0.0001)
-model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
 pkl_file = open('class_names.pkl', 'rb')
 class_names = pickle.load(pkl_file)
@@ -55,71 +58,28 @@ train_datagen = ImageDataGenerator(
     validation_split=0.2,
 )
 
-def main():
 
+def make_prediction(img):
+    '''
+    This function takes in a image and model, and uses the model to predict the class of the image
+   '''
+    data = img_to_array(img)
+    samples = expand_dims(data, 0)
+    it = train_datagen.flow(samples, batch_size=1)
+    pred = model.predict(it)
+    indices = pred[0].argsort()[-3:][::-1]
+    return [re.split(r'(\d+)-', class_names[indices[i]])[-1] for i in range(len(indices))]
+
+
+def main():
     st.header("Predict Dog Breed")
 
     reset_random_seeds()
-    # Allow the user to upload a image of their dog
-    # tf.keras.backend.clear_session()
-    image = None
-    image = st.file_uploader("The image of your dog!", ["png", "jpg", "jpeg", 'HEIC'], key='file')
-
+    image = st.file_uploader("The image of your dog!", ["png", "jpg", "jpeg"], key='file')
     starttime = timeit.default_timer()
-
-    # tf.keras.backend.clear_session()
-    # img_width, img_height = 224, 224
-    # channels = 3
-    # InceptionV3 = applications.InceptionV3(include_top=False, input_shape=(img_width, img_height, channels),
-    #                                        weights='imagenet')
-    # def build_model():
-    #     model = Sequential()
-    #     model.add(InceptionV3)
-    #     model.add(GlobalAveragePooling2D())
-    #     model.add(Dropout(0.2))
-    #     model.add(Dense(120, activation='softmax'))
-    #     model.compile(optimizer=keras.optimizers.Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
-    #     return model
-
-    def make_prediction(img):
-        '''
-        This function takes in a image and model, and uses the model to predict the class of the image 
-       '''
-        # img = img.convert('RGB')
-        # img = img.resize((224, 224), Image.NEAREST)
-        # img = tf.keras.preprocessing.image.img_to_array(img)
-        # img_array = tf.expand_dims(img, 0)
-        # prediction = model.predict(img_array)
-        # pred_class = np.argmax(prediction)
-        # return re.split('(\d+)-', classes[pred_class])[-1]
-
-        # tf.keras.backend.clear_session()
-        # model = build_model()
-        # model.load_weights('dog_modelv2.weights')
-
-        # st.write("Load/Compile Time (in seconds) :", timeit.default_timer() - starttime)
-        # st.write(image.name)
-        # img = img.convert('RGB')
-        # foo = img.resize((224, 224), Image.ANTIALIAS)
-        # foo.save(str(image.name))
-        # st.write(os.stat(str(image.name)).st_size)
-        data = img_to_array(img)
-        # new_data = tf.image.resize(data, [128, 128])
-        # new_data = tf.image.resize(data, [224, 224])
-        samples = expand_dims(data, 0)
-        it = train_datagen.flow(samples, batch_size=1)
-        pred = model.predict(it)
-        score = tf.nn.softmax(pred[0])
-        score_array = score.numpy()
-        indices = score_array.argsort()[-3:][::-1]
-        # tf.keras.backend.clear_session()
-        st.write("Load/Compile Time (in seconds) :", timeit.default_timer() - starttime)
-        # return re.split(r'(\d+)-', class_names[indices[0]])[-1]
-        return [re.split(r'(\d+)-', class_names[indices[i]])[-1] for i in range(len(indices))]
 
     if image is not None:  # if the image is an actual file then
         col1, col2 = st.beta_columns(2)  # split our layout into two columns
-        # st.balloons()  # display the ballons
         with col1:  # the first column in our layout will display the image
             image_to_share = Image.open(image)
             resized_image = image_to_share.resize((224, 224), Image.ANTIALIAS)
@@ -132,10 +92,10 @@ def main():
 
             with st.spinner('Loading...'):
                 predicted_class = make_prediction(resized_image)
-                l1 = [breed.replace('_',' ').replace('-',' ') for breed in predicted_class]
+                l1 = [breed.replace('_', ' ').replace('-', ' ') for breed in predicted_class]
                 l2 = [re.sub(r'\b[a-z]', lambda m: m.group().upper(), i) for i in l1]
-                l3 = [breed.replace('And','and') for breed in l2]
+                l3 = [breed.replace('And', 'and') for breed in l2]
+                st.write("Load/Compile Time (in seconds) :", timeit.default_timer() - starttime)
                 st.write('## **1.{}**'.format(l3[0]))
                 st.write('## **2.{}**'.format(l3[1]))
                 st.write('## **3.{}**'.format(l3[2]))
-            image = None
